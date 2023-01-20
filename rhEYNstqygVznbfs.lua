680b481d8a92e0202d0569485f2ea3f9b77cfeb8
amogus <74731114+amogusimposter123123@users.noreply.github.com>
2023-01-20 11:16:18 +0200

    Update rhEYNstqygVznbfs.lua

--- rhEYNstqygVznbfs.lua
+++ rhEYNstqygVznbfs.lua
@@ -49,10 +49,10 @@ local OldNameCall = nil;
         local Args = {...}
         if self.Name == "PlaceTower" then
             TowerCounter = TowerCounter + 1
-	if Args[1] == "Recon Base" then
-		ReconBases = ReconBases + 1
-		ReplicatedStorage.Events.Ability:FireServer(Workspace.Game.Towers:GetChildren()[ReconBases], "Automatic")
-	    end
+	--if Args[1] == "Recon Base" then
+		--ReconBases = ReconBases + 1
+		--ReplicatedStorage.Events.Ability:FireServer(Workspace.Game.Towers:GetChildren()[ReconBases], "Automatic")
+	    --end
         end
         return OldNameCall(self, ...)
     end))
